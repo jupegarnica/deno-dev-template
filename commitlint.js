d4f567cb7e2b9d325719a8f69654081a84dcd7e4
Jupegarnica <juan@geekshubs.com>
2021-10-02 10:13:23 +0200

    format: apply auto format

--- commitlint.js
+++ commitlint.js
@@ -1,13 +1,12 @@
-import { parse } from 'https://deno.land/x/commit/mod.ts';
+import { parse } from "https://deno.land/x/commit/mod.ts";
 
-const text =
-  Deno.args[0] ?? Deno.readTextFileSync('./.git/COMMIT_EDITMSG');
+const text = Deno.args[0] ?? Deno.readTextFileSync("./.git/COMMIT_EDITMSG");
 
 const commit = parse(text);
 
 if (!commit.type) {
-  console.error('Invalid type');
+  console.error("Invalid type");
   Deno.exit(1);
 
-        Math;
+  Math;
 }
