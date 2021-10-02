739f47fa1595c1495ede15ea72a720a6280508ea
Jupegarnica <juan@geekshubs.com>
2021-10-02 10:00:37 +0200

    test: test post commit

--- commitlint.ts
+++ commitlint.ts
@@ -5,6 +5,6 @@ const text = Deno.args[0] ?? Deno.readTextFileSync("./.git/COMMIT_EDITMSG");
 const commit = parse(text);
 
 if (!commit.type) {
-        console.error("Invalid type");
+    console.error("Invalid type");
   Deno.exit(1);
 }
