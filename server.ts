be302b0c14e6d0b2efcf55a8c3ea60c0e44c7fce
Jupegarnica <juan@geekshubs.com>
2021-10-02 13:47:55 +0000

    feat: playing deployctl

--- server.ts
+++ server.ts
@@ -3,6 +3,8 @@
 const listener = Deno.listen({ port: 8080 });
 if (!Deno.env.get("DENO_DEPLOYMENT_ID")) {
   const { hostname, port } = listener.addr;
+  // console.log(listener.addr);
+
   console.log(`HTTP server listening on http://${hostname}:${port}`);
 }
 
@@ -15,7 +17,7 @@ async function handleConn(conn: Deno.Conn) {
 
 async function handler(request: Request, conn: Deno.Conn) {
   const { href, origin, host, pathname, hash, search } = new URL(request.url);
-  console.log({ href, origin, host, pathname, hash, search });
+  console.log({ href, origin, host, pathname, hash, search,headers:request.headers });
 
   const readme = await Deno.readTextFile("./README.md");
 
